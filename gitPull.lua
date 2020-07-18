de7f51b286752e7f519081bec40214986407daf7
joshinils <joshua.laskowski@web.de>
2020-07-18 15:12:31 +0200

    replace startup

--- gitPull.lua
+++ gitPull.lua
@@ -2,5 +2,7 @@ print("pulling new files")
 
 shell.run("rom/programs/delete.lua","treppe.lua")
 shell.run("rom/programs/http/wget.lua","https://raw.githubusercontent.com/joshinils/turtleCode/master/treppe.lua")
+shell.run("rom/programs/delete.lua","startup.lua")
+shell.run("rom/programs/http/wget.lua","https://raw.githubusercontent.com/joshinils/turtleCode/master/startup.lua")
 
 print("Your branch is up to date with 'origin/master'.")
