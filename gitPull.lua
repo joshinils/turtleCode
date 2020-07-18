988b9b9a881b89e5577a7af5e88522b5698bdcd3
joshinils <joshua.laskowski@web.de>
2020-07-18 15:10:14 +0200

    everything works

--- gitPull.lua
+++ gitPull.lua
@@ -1,10 +1,6 @@
 print("pulling new files")
 
-print("\n")
-
 shell.run("rom/programs/delete.lua","treppe.lua")
 shell.run("rom/programs/http/wget.lua","https://raw.githubusercontent.com/joshinils/turtleCode/master/treppe.lua")
 
-print("\n")
-shell.run("rom/programs/ls.lua")
-
+print("Your branch is up to date with 'origin/master'.")
--- startup.lua
+++ startup.lua
@@ -1,4 +1,6 @@
 shell.run("rom/programs/delete.lua","gitPull.lua")
 shell.run("rom/programs/http/wget.lua","https://raw.githubusercontent.com/joshinils/turtleCode/master/gitPull.lua")
 shell.run("gitPull.lua")
+
+print("attempting to call startupCustom.lua")
 shell.run("startupCustom.lua")
