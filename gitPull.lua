ec28777de54c9d4129342e8f47b3687403f22381
joshinils <joshua.laskowski@web.de>
2020-07-18 15:06:44 +0200

    debug?

--- gitPull.lua
+++ gitPull.lua
@@ -1,4 +1,10 @@
 print("pulling new files")
 
+print("\n")
+
 shell.run("rom/programs/delete.lua","treppe.lua")
 shell.run("rom/programs/http/wget.lua","https://raw.githubusercontent.com/joshinils/turtleCode/master/treppe.lua")
+
+print("\n")
+shell.run("rom/programs/ls.lua")
+
