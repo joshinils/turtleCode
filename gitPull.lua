e73e5451dda203fd499256cecce9d1c13a2fde51
joshinils <joshua.laskowski@web.de>
2020-07-18 15:04:20 +0200

    remove before wget!

--- gitPull.lua
+++ gitPull.lua
@@ -1,3 +1,4 @@
 print("pulling new files")
 
+shell.run("rom/programs/delete.lua","treppe.lua")
 shell.run("rom/programs/http/wget.lua","https://raw.githubusercontent.com/joshinils/turtleCode/master/treppe.lua")
